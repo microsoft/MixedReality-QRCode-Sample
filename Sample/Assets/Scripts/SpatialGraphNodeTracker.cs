a74e1a9db02538124047fe2907543464af9a04a6
yl-msft <yl@microsoft.com>
2022-09-02 15:27:35 -0700

    Add OpenXR support to the project.

--- Sample/Assets/Scripts/SpatialGraphNodeTracker.cs
+++ Sample/Assets/Scripts/SpatialGraphNodeTracker.cs
@@ -4,9 +4,13 @@
 using UnityEngine;
 using Microsoft.MixedReality.Toolkit.Utilities;
 
+#if MIXED_REALITY_OPENXR
+using Microsoft.MixedReality.OpenXR;
+#else
 using SpatialGraphNode = Microsoft.MixedReality.SampleQRCodes.WindowsXR.SpatialGraphNode;
+#endif
 
-namespace Microsoft.MixedReality.SampleQRCodes
+namespace SampleQRCodes
 {
     internal class SpatialGraphNodeTracker : MonoBehaviour
     {
@@ -24,7 +28,11 @@ namespace Microsoft.MixedReality.SampleQRCodes
 
             if (node != null)
             {
+#if MIXED_REALITY_OPENXR
+                if (node.TryLocate(FrameTime.OnUpdate, out Pose pose))
+#else
                 if (node.TryLocate(out Pose pose))
+#endif
                 {
                     // If there is a parent to the camera that means we are using teleport and we should not apply the teleport
                     // to these objects so apply the inverse
